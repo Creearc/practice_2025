e49e31efbd73625e678c7d1a3027f92d02af1faa
Sharoglazov <148006285+Sharoglazov@users.noreply.github.com>
2025-07-05 20:26:39 +0500

    Add files via upload
    Хедер для voice.cpp

---
+++ Voice_Capture/Public/voice.h
@@ -0,0 +1,21 @@
+/*
+For now, unless I will be able to find a way to include the lib in there directly,
+to test and to use the class, you will have to link Vosk lib to the whole project by yourself
+*/
+//NOTE: commentary will be in english because stinky github doesn't understand russian font
+#pragma once
+#include <string>
+#include "CoreMinimal.h"
+#include "vosk_api.h" //the header file for the library
+class voice
+{
+public:
+	voice(); //constructor
+	voice(short t); //constructor with parameters
+	voice(short t, const char* modelPath); // Additional constructor for absolute path to models
+	~voice(); //destructor
+	std::string convertAudio(const char* fileLoc); //main function
+private:
+	VoskModel* model; //model that recognizes audio on specific language (depends on used model files)
+	VoskRecognizer* recognizer; //recognizer object, recognizes text in audio
+};
